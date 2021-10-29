34cfd1852255ce8112df4c92b8046cce3fa5eec2
Samuel <s.weber@webu.coop>
2021-10-29 18:10:31 +0200

    fix+add base template for topic

--- dalec_discourse/proxy.py
+++ dalec_discourse/proxy.py
@@ -12,8 +12,8 @@ from pydiscourse import DiscourseClient
 
 client = DiscourseClient(
         settings.DALEC_DISCOURSE_BASE_URL,
-        api_username=settings.DALEC_GITLAB_API_USERNAME,
-        api_key=settings.DALEC_GITLAB_API_KEY,
+        api_username=settings.DALEC_DISCOURSE_API_USERNAME,
+        api_key=settings.DALEC_DISCOURSE_API_KEY,
         )
 
 
@@ -27,7 +27,27 @@ class DiscourseProxy(Proxy):
     def _fetch(
         self, nb: int, content_type: str, channel: str, channel_object: str
     ) -> Dict[str, dict]:
-        # TODO
+        if content_type == "topic":
+            return self._fetch_topic(nb, channel, channel_object)
 
         raise ValueError("Invalid content_type %s" % content_type)
+
+    def _fetch_topic(self, nb, channel=None, channel_object=None):
+        options = {"per_page": nb}
+
+        topics = client.category_latest_topics(
+                name=channel_object,
+                parent=channel
+                )["topic_list"]["topics"]
+
+        contents = {}
+        for topic in topics:
+            contents[topic["id"]] = {
+                    **topic,
+                    "last_update_dt": now(),
+                    "creation_dt": topic["created_at"]
+                    }
+        return contents
+
+
 
