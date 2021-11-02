2a08686396310d44a91da27f82e8146f78fbb2f5
Samuel <s.weber@webu.coop>
2021-11-02 15:50:00 +0100

    proxy: last topic or category

--- dalec_discourse/proxy.py
+++ dalec_discourse/proxy.py
@@ -28,26 +28,59 @@ class DiscourseProxy(Proxy):
         self, nb: int, content_type: str, channel: str, channel_object: str
     ) -> Dict[str, dict]:
         if content_type == "topic":
-            return self._fetch_topic(nb, channel, channel_object)
+            return self._fetch_latest_topic(nb, channel, channel_object)
+        elif content_type == "category":
+            return self._fetch_categories(nb, channel, channel_object)
 
-        raise ValueError("Invalid content_type %s" % content_type)
+        raise ValueError(f"Invalid content_type {content_type}. Accepted: topic, category." )
 
-    def _fetch_topic(self, nb, channel=None, channel_object=None):
-        options = {"per_page": nb}
+    def _fetch_latest_topic(self, nb, channel=None, channel_object=None):
+        """
+        Get latest topics from entire forum or category
+        """
+        options = {
+                "per_page": nb,
+                "override_request_kwargs": {"allow_redirects": True}
+                }
 
-        topics = client.category_latest_topics(
-                name=channel_object,
-                parent=channel
-                )["topic_list"]["topics"]
+        if channel == "category" and channel_object is not None:
+            topics = client.category_latest_topics(
+                    name=channel_object,
+                    **options
+                    )["topic_list"]["topics"]
+        else:
+            topics = client.latest_topics(
+                    name=channel_object,
+                    **options,
+                    )["topic_list"]["topics"]
+
 
         contents = {}
         for topic in topics:
             contents[topic["id"]] = {
                     **topic,
+                    "base_url": settings.DALEC_DISCOURSE_BASE_URL,  # to reconstruct url later
                     "last_update_dt": now(),
                     "creation_dt": topic["created_at"]
                     }
         return contents
+
+    def _fetch_categories(self, nb, channel=None, channel_object=None):
+        """
+        Get categories
+        """
+        categories = client.categories()
+
+        contents = {}
+        for category in categories:
+            contents[category["id"]] = {
+                    **category,
+                    "base_url": settings.DALEC_DISCOURSE_BASE_URL,  # to reconstruct url later
+                    "last_update_dt": now(),
+                    "creation_dt": now()
+                    }
+        return contents
+
 
 
 
